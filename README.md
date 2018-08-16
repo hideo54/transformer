961c9ab4c45bf81d56a64aa7a09159fadf3ecd7b
hideo54 <hideo54@hideo54.com>
2018-08-16 19:30:36 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# transformer
+
+アップロードされた画像に背景をつけて任意のサイズの画像で返すWebアプリケーションです。
+
+[squarify](https://github.com/hideo54/squarify)の亜種。
+
+## 特徴
+
+ボタン押すだけラクラク
+
+## 注意
+
+雑実装。同時に複数人が使うと他人の上げた画像が返ってくる可能性がある。
+
+## How to use
+
+1. `yarn` (or npm if you like)
+1. `pm2 start app.js` (or another if you like)
+1. Visit http://[address]:1235
---
+++ app.js
@@ -0,0 +1,50 @@
+const Koa = require('koa');
+const app = new Koa();
+const route = require('koa-route');
+const multer = require('koa-multer');
+const fs = require('fs');
+const pug = require('pug');
+const uglifycss = require('uglifycss');
+
+const sharp = require('sharp');
+const returnPNG = async (file, size, isTransparent) => {
+    const alpha = isTransparent ? 0 : 1;
+    await sharp(file).background( { r: 255, g: 255, b: 255, alpha: alpha } )
+        .resize(size.width, size.height)
+        .embed()
+        .png()
+        .toFile('output.png');
+};
+
+app.use(route.post('/download', multer().single('file')));
+app.use(async (ctx, next) => {
+    if (ctx.path === '/') {
+        ctx.status = 200;
+        ctx.body = pug.renderFile('index.pug');
+    } else if (ctx.path === '/download') {
+        if (ctx.method === 'GET') {
+            ctx.status = 303;
+            ctx.body = '<head><meta http-equiv="refresh" content="0; URL=/" /></head>';
+        }
+        if (ctx.method === 'POST') {
+            const buf = ctx.req.file.buffer;
+            const size = { width: Number(ctx.req.body.width), height: Number(ctx.req.body.height) };
+            const isTransparent = ctx.req.body.radio === 'transparent' ? true : false;
+            await returnPNG(buf, size, isTransparent);
+            ctx.status = 200;
+            ctx.body = pug.renderFile('download.pug');
+        }
+    } else if (ctx.path === '/output') {
+        ctx.status = 200;
+        ctx.type = 'image/png';
+        ctx.body = fs.createReadStream('output.png');
+    } else if (ctx.path === '/style.css') {
+        ctx.status = 200;
+        ctx.type = 'text/css';
+        ctx.body = uglifycss.processFiles(['style.css']);
+    } else {
+        ctx.status = 500;
+    }
+});
+
+app.listen(1235);
